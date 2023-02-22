a11fe21a6b343ff31b5b6fb07e3a75b8aa6ab5d5
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2023-02-22 20:53:44 -0300

    Add files via upload

---
+++ src/models/Cliente.js
@@ -0,0 +1,10 @@
+class Cliente {
+    constructor(nome, email, tel, end) {
+        this.nome = nome,
+        this.email = email,
+        this.tel = tel,
+        this.end = end
+    }
+}
+
+module.exports = Cliente
