45c80e072d0661123d68572a5b3c33193820ae96
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2023-02-22 20:54:50 -0300

    Add files via upload

--- src/controllers/controller-cliente.js
+++ src/controllers/controller-cliente.js
@@ -1,8 +1,18 @@
-class ControllerCliente {         // Classe
-    static rotas(app) {          // metado
-    app.get('/cliente', (req, res) => {        // Roda do Cliente
-    res.send("Rota GET do Cliente ativada")
-    })
+const Cliente = require('../models/Cliente')
+
+  class ControllerCliente {         // Classe
+    static rotas(app) {          
+    app.get('/cliente', ControllerCliente.listarCliente)      // Chamada da rota Get
+    app.post('/cliente', ControllerCliente.cadastrarCliente)   // Chamada da rota Post
+  }
+
+  static listarCliente(req, res) {              // Get
+    res.send("Rota Get do Cliente Falida")
+  }
+
+  static cadastrarCliente(req, res) {    // Post
+    const cliente = new Cliente(req.body.nome, req.body.email, req.body.tel, req.body.end)
+    res.send(cliente)     // Responde a requisição
   }
 }
 
--- src/controllers/controller-livro.js
+++ src/controllers/controller-livro.js
@@ -1,8 +1,19 @@
+const Livro = require('../models/Livro')
+
 class ControllerLivro {
     static rotas(app) {
-    app.get('/livro', (req, res) => {
-    res.send("Rota GET do Livro ativada")
-    })
+    app.get('/livro', ControllerLivro.listarLivro)
+    app.post('/livro', ControllerLivro.cadastrarLivro)
+  }
+
+  static listarLivro(req, res) {
+    res.send("Rota Get do Livro ativo")
+  }
+
+  static cadastrarLivro(req, res) {
+    const livro = new Livro(req.body.genero, req.body.diretora)
+    res.send(livro)
   }
 }
+  
 module.exports = ControllerLivro
