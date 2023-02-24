43a47028bb808402bc7f636bb35fd2981cf4de78
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2023-02-24 15:00:12 -0300

    adicionei a rota delete

--- src/controllers/controller-cliente.js
+++ src/controllers/controller-cliente.js
@@ -7,6 +7,7 @@ const { bdCliente } = require('../estrutura/bd')
     app.get('/cliente', ControllerCliente.listarCliente)      // Chamada da rota Get
     app.get('/cliente/:email', ControllerCliente.buscarClienteEmail)
     app.post('/cliente', ControllerCliente.cadastrarCliente)   // Chamada da rota Post
+    app.delete('/cliente/:email', ControllerCliente.deletarCliente)   // chamada da rota Delete
   }
 
   static listarCliente(req, res) {     // Get
@@ -29,6 +30,21 @@ const { bdCliente } = require('../estrutura/bd')
     const cliente = new Cliente(req.body.nome, req.body.email, req.body.tel, req.body.end)
     bdCliente.push(cliente)
     res.send(bdCliente)     // Responde a requisição
+  }
+
+  static deletarCliente(req, res) {     // Delete
+    const cliente = bdCliente.find((cliente) => cliente.email === req.params.email)   //Busca o email  na lista de Clientes
+
+    if (!cliente) {   // Se o Cliente não for encontrado
+      res.send('Cliente não encontrado')
+      return
+    }
+    const index = bdCliente.indexOf(cliente)  // Se o Cliente for encontrado
+    bdCliente.splice(index, 1)  
+    res.send({   //Devolve o Cliente deletado
+      "Mensagem: ": `O cliente do email ${cliente.email} foi deletado`,
+      });
+      
   }
 }
 
