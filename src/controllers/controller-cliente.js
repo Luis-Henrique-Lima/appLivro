7904f01438d9f70112a87d55b6adf7ef568f6dc5
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2023-02-25 13:45:12 -0300

    Add files via upload

--- src/controllers/controller-cliente.js
+++ src/controllers/controller-cliente.js
@@ -7,6 +7,7 @@ const { bdCliente } = require('../estrutura/bd')
     app.get('/cliente', ControllerCliente.listarCliente)      // Chamada da rota Get
     app.get('/cliente/:email', ControllerCliente.buscarClienteEmail)
     app.post('/cliente', ControllerCliente.cadastrarCliente)   // Chamada da rota Post
+    app.put('/cliente/:email', ControllerCliente.atualizaCliente)   // chamada da rota put
     app.delete('/cliente/:email', ControllerCliente.deletarCliente)   // chamada da rota Delete
   }
 
@@ -14,22 +15,37 @@ const { bdCliente } = require('../estrutura/bd')
     res.send(bdCliente)
   }
 
-  static buscarClienteEmail(req, res) {
-    const cliente = bdCliente.find(
+  static buscarClienteEmail(req, res) {   // Get
+    const cliente = bdCliente.find(    // Buscando o email na lista de clientes
       (cliente) => cliente.email === req.params.email
     )
 
-    if (!cliente) {
-      res.rend('Cliente não encontrado')
+    if (!cliente) {     //  se o cliente nao for encontrado fala que teve um erro
+      res.rend('Cliente não encontrado')    
       return;
     }
-    res.send(cliente)
+    res.send(cliente)  // se o cliente for encontrado devilve o cliente
   }
 
   static cadastrarCliente(req, res) {    // Post
     const cliente = new Cliente(req.body.nome, req.body.email, req.body.tel, req.body.end)
     bdCliente.push(cliente)
     res.send(bdCliente)     // Responde a requisição
+  }
+
+  static atualizaCliente(req, res){  // esse é o Put
+    const cliente = bdCliente.find(cliente => cliente.email === req.params.email) //Buscar email no array cliente
+
+    if (!cliente) {   // se o cliente nao for encontrado 
+      res.send('Usuario não encontrado')
+      return
+    }
+        // Se chegar até aqui, atualiza as chaves do cliente recebendo as informações que vem do corpo da requisição 
+    cliente.nome = req.body.nome
+    cliente.email = req.body.email
+    cliente.senha = req.body.senha
+
+    res.send({"Mensagem": "Cliente atualizado com sucesso", "Dados do Cliente": cliente})
   }
 
   static deletarCliente(req, res) {     // Delete
